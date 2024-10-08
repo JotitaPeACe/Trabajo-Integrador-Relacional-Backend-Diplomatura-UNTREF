dd694ca85ac004e752433d8eb53db37731c382cd
JotitaPeACe <juampic99@gmail.com>
2024-10-08 11:43:29 -0300

    actores y generos

--- models/actor.js
+++ models/actor.js
@@ -1,2 +1,20 @@
 // Model for Actor
-    
+const { DataTypes } = require('sequelize');
+const sequelize = require('../conexion/database');  
+
+const Actor = sequelize.define('Actor', {
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  nombre: {
+    type: DataTypes.STRING(100), 
+    allowNull: false  
+  }
+}, {
+  tableName: 'actores', 
+  timestamps: false      
+});
+
--- models/genero.js
+++ models/genero.js
@@ -1,2 +1,21 @@
 // Model for Genero
-    
+const { DataTypes } = require('sequelize');
+const sequelize = require('../conexion/database');
+
+const Genero = sequelize.define('Genero', {
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  nombre: {
+    type: DataTypes.STRING(100),  
+    allowNull: false  
+  }
+}, {
+  tableName: 'generos', 
+  timestamps: false
+});
+
+module.exports = Genero;
