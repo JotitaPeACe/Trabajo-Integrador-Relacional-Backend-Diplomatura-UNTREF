297c69c1272d32a11e9f2dc01e11f18bad06ad51
JotitaPeACe <juampic99@gmail.com>
2024-10-08 11:48:05 -0300

    categoria

--- models/categoria.js
+++ models/categoria.js
@@ -1,2 +1,21 @@
 // Model for Categoria
-    
+const { DataTypes } = require('sequelize');
+const sequelize = require('../conexion/database');
+
+const Categoria = sequelize.define('Categoria', {
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  nombre: {
+    type: DataTypes.STRING(100),  
+    allowNull: false  
+  }
+}, {
+  tableName: 'categorias',
+  timestamps: false        
+});
+
+module.exports = Categoria;
