a7d132b67d09484a3cccb534df2b4e0aebd46b37
JotitaPeACe <juampic99@gmail.com>
2024-11-01 21:34:55 -0300

    database

--- conexion/database.js
+++ conexion/database.js
@@ -7,22 +7,63 @@ const ENV = process.env.NODE_ENV || 'local';
 dotenv.config({ path: `.env.${ENV}` });
 
 // Configuración de la base de datos
-const dbConfig = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-};
+const dbConfig = process.env.DATABASE_URL
+    ? {
+          url: process.env.DATABASE_URL,
+          dialect: 'mysql',
+      }
+    : {
+          database: process.env.DB_NAME,
+          username: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT,
+          dialect: 'mysql',
+      };
 
 // Creación de la instancia de Sequelize
 const sequelize = new Sequelize(dbConfig);
 
 // Autenticación con la base de datos
 sequelize.authenticate()
-  .then(() => console.log('Conectado a la base de datos de MySQL'))
-  .catch((err) => console.error('No se pudo conectar a la base de datos:', err));
+    .then(() => console.log('Conectado a la base de datos de MySQL'))
+    .catch((err) => console.error('No se pudo conectar a la base de datos:', err));
 
 // Exportación de la instancia de Sequelize
 module.exports = sequelize;
+
+// // Importaciones
+// const dotenv = require('dotenv');
+// const { Sequelize } = require('sequelize');
+
+// // Configuración de entorno
+// const ENV = process.env.NODE_ENV || 'local';
+// dotenv.config({ path: `.env.${ENV}` });
+
+// // Configuración de la base de datos
+// const dbConfig = process.env.DATABASE_URL
+//     ? {
+//           url: process.env.DATABASE_URL,
+//           dialect: 'mysql',
+//       }
+//     : {
+//           database: process.env.DB_NAME,
+//           username: process.env.DB_USER,
+//           password: process.env.DB_PASSWORD,
+//           host: process.env.DB_HOST,
+//           port: process.env.DB_PORT,
+//           dialect: 'mysql',
+//       };
+
+// // Creación de la instancia de Sequelize
+// const sequelize = process.env.DATABASE_URL ? 
+//     new Sequelize(dbConfig.url, { dialect: 'mysql' }) :
+//     new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+
+// // Autenticación con la base de datos
+// sequelize.authenticate()
+//     .then(() => console.log('Conectado a la base de datos de MySQL'))
+//     .catch((err) => console.error('No se pudo conectar a la base de datos:', err));
+
+// // Exportación de la instancia de Sequelize
+// module.exports = sequelize;
